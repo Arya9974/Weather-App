a7e8486631145630528bda561da9cc8204004be8
Arya9974 <157284556+Arya9974@users.noreply.github.com>
2024-09-09 17:34:51 +0330

    Add files via upload

--- weather app/assets/main/js/main.js
+++ weather app/assets/main/js/main.js
@@ -1,59 +1,47 @@
-let valeusearch = document.getElementById('valeusearch');
-let city = document.getElementById('city');
-let temp = document.getElementById('temp');
-let desceription = document.querySelector('.description')
-let clouds = document.getElementById('clouds')
-let humidity = document.getElementById('humidity')
-let pressure = document.getElementById('pressure')
-let form = document.querySelector('form')
-let main = document.querySelector('main')
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (valeusearch.value != '') {
+let id = '9505fd1df737e20152fbd78cdb289b6a';
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
+let city = document.querySelector('.name');
+let form = document.querySelector("form");
+let temperature = document.querySelector('.temperature');
+let description = document.querySelector('.description');
+let valueSearch = document.getElementById('name');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
+let main = document.querySelector('main');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();  
+    if(valueSearch.value != ''){
         searchWeather();
     }
-})
-
-let id = 'fff6238b9b03d8dc9c297487b5adad14';
-
-
-let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
-
+});
 const searchWeather = () => {
-
-    fetch(url + '&q=' + valeusearch.value)
-        .then(responsive => responsive.json())
+    fetch(url+'&q='+ valueSearch.value)
+        .then(response => response.json())
         .then(data => {
             console.log(data);
-            if (data.cod == 200) {
-                city.querySelector('figcaption').innerText = data.name;
+            if(data.cod == 200){
+                city.querySelector('figcaption').innerHTML = data.name;
                 city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                temperature.querySelector('span').innerText = data.main.temp;
+                description.innerText = data.weather[0].description;
 
-                temp.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-                temp.querySelector('figcaption span').innerText = data.main.temp;
-                desceription.innerText = data.weather[0].desceription;
                 clouds.innerText = data.clouds.all;
                 humidity.innerText = data.main.humidity;
-                pressure.innerText = data.main.pressure
-
-
-            } else {
-                //false
+                pressure.innerText = data.main.pressure;
+            }else{
                 main.classList.add('error');
                 setTimeout(() => {
-                    main.classList.remove('error')
-                }
-                ,1000)
+                    main.classList.remove('error');
+                }, 1000);
             }
-
-            valeusearch.value = '';
-
+            valueSearch.value = '';
         })
 }
 
-const initapp = () => {
-    valeusearch.value = 'Tehran'
+const initApp = () => {
+    valueSearch.value = 'Washington';
     searchWeather();
 }
-
-initapp(); 
+initApp();
